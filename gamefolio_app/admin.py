1b6e9752121a97775c3572fba9c8558c1f0a18be
“Dylan <100671823+dhdylanhuang@users.noreply.github.com>
2024-03-01 15:02:24 +0000

    Updated gamefolio_app

--- gamefolio_app/admin.py
+++ gamefolio_app/admin.py
@@ -1,3 +1,60 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models import Avg, Count
+from gamefolio_app.models import Author, Game, Review, List, ListEntry
 
-# Register your models here.
+
+class ListsInLine(admin.TabularInline):
+    model = List
+    classes = ['collapse']
+
+class ReviewInLine(admin.TabularInline):
+    model = Review
+    classes = ['collapse']
+
+#Shows list entrys on list page in admin
+class ListEntry(admin.TabularInline):
+    model = ListEntry
+    classes = ['collapse']
+
+class ListAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            "List Information",
+            {
+                "fields": ["title", "description", "slug"],
+            }
+        ),
+    ]
+    readonly_fields = ('slug',)
+    inlines = (ListEntry,)
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'average_rating', 'total_reviews')
+    readonly_fields = ('average_rating', 'total_reviews',)
+
+    def average_rating(self, obj):
+        return obj.average_rating()
+    
+    def total_reviews(self, obj):
+        return obj.total_reviews()
+    
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _average_rating = Avg('review__rating'),
+            _total_rating = Count('review', distinct = True)
+        ).order_by('_average_rating')
+        return queryset
+    
+    average_rating.admin_order_field = "_average_rating"
+    total_reviews.admin_order_field = "_total_rating"
+    inlines = [ReviewInLine]
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = (ReviewInLine, ListsInLine)
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Review)
+admin.site.register(List, ListAdmin)
--- gamefolio_app/tests.py
+++ gamefolio_app/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from gamefolio_app.models import Game, Review, Author
 
-# Create your tests here.
+class ModelTests(TestCase):
+
+    def setUp(self):
+        #Generate generic test objects
+        self.game = Game(title = "Game")
+        self.game.save()
+        self.user = User.objects.create_user("Mario", "mario@mario.com", "yoshi123")
+        self.author = Author(user = self.user)
+        self.author.save()
+        
+    def tearDown(self):
+        #delete everything once done
+        self.game.delete()
+        self.user.delete()
+        self.author.delete()
+
+    #This will test that the game has a primary key that is generated as a slug
+    def test_game_slug_id(self):
+        gameOne = Game(title = "Test Game")
+        gameOne.save()
+        gameTwo = Game(title = "Test Game")
+        gameTwo.save()
+        self.assertEquals(gameOne.id, 'test-game')
+        self.assertEqual(gameTwo.id, 'test-game-1')
+
+    #Ensures that rating can not be above 10 or below 1
+    def test_review_values(self):
+        review = Review(user = self.author, game = self.game, content = "This is a random review", rating = -1)
+        review.save()
+        self.assertEqual(review.rating, 1)
+        review = Review(user = self.author, game = self.game, content = "This is a random review", rating = 11)
+        review.save()
+        self.assertEqual(review.rating, 10)
