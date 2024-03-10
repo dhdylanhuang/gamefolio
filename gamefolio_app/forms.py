ba72d687d1c8abf3749b88a4078199315743953d
Dylan <dhdylanhuang@gmail.com>
2024-03-10 17:52:53 +0000

    improved forms

--- gamefolio_app/forms.py
+++ gamefolio_app/forms.py
@@ -2,16 +2,41 @@ from django import forms
 from django.contrib.auth.models import User
 from gamefolio_app.models import Author
 
-
-
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'username': 'Username',
+            'email': 'Email',
+            'password': 'Password',
+        }
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        
+        return cleaned_data
 
 class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
         fields = ('website', 'picture',)
-
+        widgets = {
+            'website': forms.URLInput(attrs={'class': 'form-control'}),
+            'picture': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'website': 'Website',
+            'picture': 'Profile Picture',
+        }
