e4c0f9b86e2e6f9e2ce3d747e7269ceb52214a05
Dylan <100671823+dhdylanhuang@users.noreply.github.com>
2024-03-25 13:55:11 +0000

    Updated population script to match models

--- populate_gamefolio.py
+++ populate_gamefolio.py
@@ -27,6 +27,9 @@ RESULTS_PER_QUERY = 500  #Max results per query is 500
 NUMBER_OF_USERS = 100
 descriptors = ["Little", "Big", "Small", "Hidden", "Crazy", "Blue", "Red", "Green", "Yellow", "Rainbow", "Silly"]
 usernames = ["Mario", "Link", "Zelda", "Luigi", "Yoshi", "DonkeyKong", "Sonic", "Peach", "Steve", "Mastercheif", "Pikachu", "Goomba", "Bowser"]
+bios = ["I love gaming.", "Gaming enthusiast.", "Casual gamer.", "Hardcore gamer.", "I live to game.", "Gaming is my passion.", 
+        "Always looking for the next great game.", "Gaming is my life.", "I can't imagine life without gaming.", "Gaming is my escape from reality.",
+        "Some ordinary gamer doing ordinary things.", "This web app development group did a very good job"]
 
 ### Review Parameters ###
 AVG_REVIEWS_PER_GAME = 10
@@ -53,8 +56,9 @@ list_names = ["Worst games of all time", "{username}'s list of best games of all
 
 ###########################################################- Population and Deletion Methods -###########################################################
 
-def populate_games():
-    print("Populating games...")
+def populate_games(print_messages = False):
+    if(print_messages):
+        print("Populating games...")
     query_count = 0
     for games_left in range(NUMBER_OF_GAMES, 0, -RESULTS_PER_QUERY):
 
@@ -71,6 +75,7 @@ def populate_games():
         response = requests.post('https://api.igdb.com/v4/games', **BASE_REQUEST)
         games_data = response.json()
 
+        games_to_add = []
         for entry in games_data:
             try:
                 name = entry['name'] 
@@ -83,34 +88,46 @@ def populate_games():
                     picture = entry['cover']['image_id']
 
                 game = Game(id = slug, title = name, description = description, genre = genre, pictureId = picture)
-                game.save()
+                games_to_add.append(game)
             except Exception as e:
                 print(e)
                 continue
+        Game.objects.bulk_create(games_to_add)
             
         query_count+=1
-        print(f"{Game.objects.count()}/{NUMBER_OF_GAMES}")
-    print("Games populated!")
+        if(print_messages):
+            print(f"{Game.objects.count()}/{NUMBER_OF_GAMES}")
+        
+    if(print_messages):
+     print("Games populated!")
 
-def populate_users():
-    print("Populating authors and users...")
+def populate_users(print_messages = False):
+    
+    if(print_messages):
+        print("Populating authors and users...")
+    authors_batch = []
     for i in range(NUMBER_OF_USERS):
         try:
             username = random.choice(descriptors) +  random.choice(usernames) + str(random.randint(1000, 9999))
             password = username #use username for password so it is easy to sign in to test account
             email = username +"@email.com" #fake email
+            bio = random.choice(bios)
             user = User.objects.create_user(username, email, password)
-
-            author = Author(user = user)
-            author.save()
+            author = Author(user = user, bio = bio)
+            authors_batch.append(author)
         except Exception as e:
             print(e)
             continue
-    print(f"User and Author populated with {Author.objects.count()} entries!")
+    Author.objects.bulk_create(authors_batch)
+    
+    if(print_messages):
+        print(f"User and Author populated with {Author.objects.count()} entries!")
 
-def populate_reviews():
-    print("Populating reviews...")
+def populate_reviews(print_messages = False):
+    if(print_messages):
+        print("Populating reviews...")
     number_of_users = User.objects.count()
+    reviews_batch = []
     for game in Game.objects.all():
 
         avg_rating = random.randint(1, 10)
@@ -125,21 +142,29 @@ def populate_reviews():
         for author in authors:             
             try:                    
                 content = random.choice(reviews)                                          #Generate random content for review
-                rating = int(random.normal(loc = avg_rating, scale = 2))                  #Generates reviews normally around a point for more realism
+                rating = max(1, min(int(random.normal(loc = avg_rating, scale = 2)), 10)) #Generates reviews normally around a point for more realism
                 views = random.randint(0, game.views)                                     #A review shoudln't have more views than the game
                 likes = int(views * random.random()*0.5)                                  #Likes will be a percentage of the views
                 random_date = end_date - timedelta(random.randint(1,1000))                #Generate random date
                 datePosted = random_date
                 
                 review = Review(author = author, game = game, rating = rating, content = content, likes = likes, views = views, datePosted = datePosted)
-                review.save()
+                reviews_batch.append(review)
             except Exception as e:
                 print(e)
                 continue
-    print(f"Reviews populated with {Review.objects.count()} entries!")
+            if(len(reviews_batch) > 5000):
+                Review.objects.bulk_create(reviews_batch)
+                reviews_batch.clear()
+                
+    if(print_messages):
+        print(f"Reviews populated with {Review.objects.count()} entries!")
 
-def populate_lists():
-    print("Populating lists and List Entries...")
+def populate_lists(print_messages = False):
+    
+    if(print_messages):
+        print("Populating lists and List Entries...")
+    list_entry_batch = []
     for author in Author.objects.all():
         #Not every user is gonna have lists
         if(random.random() > USER_WITH_LIST_PERCENT):
@@ -150,7 +175,7 @@ def populate_lists():
                 #Make List
                 list_name = random.choice(list_names).format(username = author.user.username)
                 description = random.choice(["", "This is just a placeholder list description rather than nothing."])
-                list = List(author = author, title = list_name, description = description)
+                list = List(author = author, title = list_name, description = description, views = random.randint(0, 1000))
                 list.save()
                 
                 #Add entries
@@ -159,18 +184,20 @@ def populate_lists():
 
                 for game in games:
                     listEntry = ListEntry(list = list, game = game)
-                    listEntry.save()
+                    list_entry_batch.append(listEntry)
             except Exception as e:
                 print(e)
                 continue
-
-    print(f"List populated with {List.objects.count()} entries!")
-    print(f"List Entries populated with {ListEntry.objects.count()} entries!")
+    ListEntry.objects.bulk_create(list_entry_batch)
+    
+    if(print_messages):
+        print(f"List populated with {List.objects.count()} entries!")
+        print(f"List Entries populated with {ListEntry.objects.count()} entries!")
 
 def clear_database():
     print("Deleting all records...")
     print("This may take a while")
-    ENTRIES_DELETED = 500
+    ENTRIES_DELETED = 5000
     try:
         count = 0
 
@@ -238,11 +265,11 @@ def populate():
     if confirm("Are you sure you want to populate the database?"):
         random.seed(SEED)
         print("Populating the database...")
-        populate_games()
+        populate_games(True)
         if all:
-            populate_users()
-            populate_reviews()
-            populate_lists()
+            populate_users(True)
+            populate_reviews(True)
+            populate_lists(True)
     else:
         print("Exiting the population program!")
 
