959387f846ad168f427f0f73778f080020d63370
Dylan <100671823+dhdylanhuang@users.noreply.github.com>
2024-03-26 09:20:05 +0000

    Game page view

--- gamefolio_app/views.py
+++ gamefolio_app/views.py
@@ -146,6 +146,71 @@ class ListProfilesView(View):
             
         return render(request,'gamefolio_app/list_profiles.html',{'authors': profiles})
     
+class GamePageView(View):
+    def get(self, request, game_id):
+        game = get_object_or_404(Game, id=game_id)
+        if request.user.is_authenticated:
+            reviews = Review.objects.filter(game=game).exclude(author__user=request.user)
+            user_reviews = Review.objects.filter(game=game).filter(author__user=request.user)
+        else:
+            reviews = Review.objects.filter(game=game)
+            user_reviews = Review.objects.none()
+        related_games = Game.objects.filter(genre=game.genre).exclude(id=game_id).order_by('?')[:4]
+
+        if(len(related_games) < 4):
+            related_games = Game.objects.all().exclude(id=game_id).order_by('?')[:4]
+  
+        sort_reviews_by = request.GET.get('sort_reviews', 'recent')
+        
+        if sort_reviews_by == 'liked':
+            reviews = reviews.annotate(likes_total=Sum('likes')).order_by('-likes_total', '-datePosted')
+        elif sort_reviews_by == 'recent':
+            reviews = reviews.order_by('-datePosted')
+        elif sort_reviews_by == 'rating':
+            reviews = reviews.order_by('-rating')
+        
+        form = ReviewForm()
+
+        lists_to_add=[]
+        if(request.user.is_authenticated):   
+            game = Game.objects.get(id=game_id)
+            lists = List.objects.filter(author__user=request.user)
+            entries = ListEntry.objects.filter(list__in = lists, game = game).values("list_id")
+            lists_to_add = lists.exclude(pk__in=entries)
+
+        context = {
+            'game': game,
+            'reviews': reviews,
+            "user_reviews": user_reviews,
+            'form': form,  
+            "review_ratings": get_game_ratings(game_id),
+            'related_games': related_games,
+            "stype": sort_reviews_by,
+            "lists_to_add": lists_to_add,
+        }
+        return render(request, 'gamefolio_app/game.html', context)
+
+    def post(self, request, game_id):
+        game = get_object_or_404(Game, id=game_id)
+        reviews = Review.objects.filter(game=game_id)
+
+        form = ReviewForm(request.POST)  
+
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.game = game  
+            review.author = request.user.author  
+            review.save()
+            return redirect('gamefolio_app:game', game_id=game_id)
+        
+        context = {
+            'game': game,
+            'reviews': reviews,
+            'form': form,
+            "review_ratings": get_game_ratings(game_id),
+        }
+        return render(request, 'gamefolio_app/game.html', context)
+    
 #Helper Functions
 def handler404(request, exception, template_name="gamefolio_app/404.html"):
     response = render_to_response(template_name)
