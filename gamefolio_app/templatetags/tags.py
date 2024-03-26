647af6ace31fab087819708404d604ddfd170bff
Dylan <100671823+dhdylanhuang@users.noreply.github.com>
2024-03-26 09:15:04 +0000

    Updated template tages

--- gamefolio_app/templatetags/tags.py
+++ gamefolio_app/templatetags/tags.py
@@ -1,7 +1,24 @@
 from django import template
 
+from gamefolio_app.models import ListEntry
+
 register = template.Library()
 
 @register.simple_tag
 def get_image(game, image_type):
     return game.get_image(image_type)
+
+@register.inclusion_tag("gamefolio_app/list_image.html")
+def render_list_images(list):
+    entries = ListEntry.objects.filter(list = list)[:4];
+    if(len(entries) < 4 and len(entries) > 0):
+        entries =[entries[0]];
+    return {"entries": entries, "list": list}
+
+@register.inclusion_tag("gamefolio_app/game_card.html")
+def render_game_card(game, *args, **kwargs):
+    try:
+        verbose = kwargs["verbose"]
+    except:
+        verbose = True
+    return {"game": game, "verbose":verbose}
