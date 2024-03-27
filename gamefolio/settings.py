044d280826189f0a40b2548890dee6d24325176d
Dylan <100671823+dhdylanhuang@users.noreply.github.com>
2024-03-27 16:45:16 +0000

    Hosting

--- gamefolio/settings.py
+++ gamefolio/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = '68y1in%#_2lwq2w^qw3tm!rpw@=3wgnm82rwyp_amf_!akh3c9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 
-                 '127.0.0.1',]
+ALLOWED_HOSTS = ['dhdylanhuang.eu.pythonanywhere.com',]
 
 
 # Registration config
