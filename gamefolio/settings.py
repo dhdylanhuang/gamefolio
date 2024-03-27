eacbb983bd206220e406fd1b644a56e2973388ff
Dylan <100671823+dhdylanhuang@users.noreply.github.com>
2024-03-27 15:33:37 +0000

    Prepping for hosting

--- gamefolio/settings.py
+++ gamefolio/settings.py
@@ -25,9 +25,10 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = '68y1in%#_2lwq2w^qw3tm!rpw@=3wgnm82rwyp_amf_!akh3c9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 
 # Registration config
 
